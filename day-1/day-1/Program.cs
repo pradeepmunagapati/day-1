ad2d8edc20507a0450adbbdad212a1ccb00f1590
pradeepmunagapati <pradeepmunagapati99@gmail.com>
2021-05-27 11:22:26 +0530

    first commit

---
+++ day-1/day-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+            Console.WriteLine(x+y);
+        }
+    }
+}
